d6145169234e20e6a3812071a5cfed77ce720c9e
Nbfinanzas <nicolas.nb2002@gmail.com>
2025-04-15 18:00:20 -0300

    realice un contador de palabras a partir de pseudocódigo y un diagrama de flujos

---
+++ contador-algo.py
@@ -0,0 +1,11 @@
+#paso 1
+
+palabra_ingresada = input("ingresa una palabra  ")
+
+#paso 2
+
+cant_letras = len(palabra_ingresada)
+
+#paso 3
+
+print("la palabra ingresada tiene", cant_letras, " letras")
